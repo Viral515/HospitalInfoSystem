1104830fb78ef2c2494a5d48bda74c8e2095638a
Viral515 <nikish515@bk.ru>
2024-09-09 03:16:24 +0700

    Create empty project.

---
+++ src/main/java/ru/egarcourses/HospitalInfoSystem/HospitalInfoSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.egarcourses.HospitalInfoSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalInfoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalInfoSystemApplication.class, args);
+	}
+
+}
